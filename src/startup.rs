d419ba71b7444484d7053e9647b3f34ced1328e0
Keith Wade <keawade@protonmail.com>
2023-06-06 10:13:59 -0400

    Compiles after adding db connection

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,20 @@
-use actix_web::{dev::Server, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 use super::routes::health_check;
 use super::routes::subscribe;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
-        .listen(listener)?
-        .run();
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
+        App::new()
+            .service(health_check)
+            .service(subscribe)
+            .app_data(connection.clone())
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
